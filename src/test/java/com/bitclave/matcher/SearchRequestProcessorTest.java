df3ef56301e3ad6759a733ac563f33bf6e2a5894
Sai Boorlagadda <sai.boorlagadda@gmail.com>
2019-01-20 19:28:51 -0800

    Added an enum deserializer to handle unknown states

--- src/test/java/com/bitclave/matcher/SearchRequestProcessorTest.java
+++ src/test/java/com/bitclave/matcher/SearchRequestProcessorTest.java
@@ -1,5 +1,23 @@
 package com.bitclave.matcher;
 
+import com.bitclave.matcher.models.Offer;
+import com.bitclave.matcher.models.OfferSearch;
+import com.bitclave.matcher.models.OfferSearchResultItem;
+import com.bitclave.matcher.models.SearchRequest;
+import com.bitclave.matcher.store.OfferStore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
 import static com.bitclave.matcher.models.OfferSearch.newOfferSearch;
 import static java.util.Collections.EMPTY_LIST;
 import static org.mockito.ArgumentMatchers.any;
@@ -8,22 +26,6 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-import com.bitclave.matcher.models.Offer;
-import com.bitclave.matcher.models.OfferSearch;
-import com.bitclave.matcher.models.OfferSearchResultItem;
-import com.bitclave.matcher.models.SearchRequest;
-import com.bitclave.matcher.store.OfferStore;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -42,6 +44,7 @@ public class SearchRequestProcessorTest {
   @After
   public void tearDown() {
     reset(baseClient);
+    reset(offerStore);
   }
 
   @Test
@@ -89,6 +92,23 @@ public class SearchRequestProcessorTest {
     SearchRequest request = new SearchRequest(1L, "owner");
     Offer offer = new Offer(1L, "owner");
     OfferSearch offerSearch = newOfferSearch(request.getId(), offer.getId());
+    OfferSearchResultItem existingOfferSearch = new OfferSearchResultItem(offerSearch, offer);
+
+    doReturn(Arrays.asList(offer)).when(offerStore).search(any());
+    doReturn(Arrays.asList(existingOfferSearch)).when(baseClient).findOfferSearch(request.getId());
+
+    processor.process(Arrays.asList(request));
+
+    verify(baseClient, never()).saveOfferSearch(any());
+  }
+
+  @Test
+  public void processorHandlesUnknownFieldsInOfferSearch() throws IOException {
+    SearchRequest request = new SearchRequest(1L, "owner");
+    Offer offer = new Offer(1L, "owner");
+
+    String offerSearchStr = "{\"offerId\":1, \"searchRequestId\": 1, \"state\": \"XXXXXX\"}";
+    OfferSearch offerSearch = new ObjectMapper().readValue(offerSearchStr, OfferSearch.class);
     OfferSearchResultItem existingOfferSearch = new OfferSearchResultItem(offerSearch, offer);
 
     doReturn(Arrays.asList(offer)).when(offerStore).search(any());
