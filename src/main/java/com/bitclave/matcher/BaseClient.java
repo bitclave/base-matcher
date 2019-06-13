46c4cba1fad0800f8f3098149b90c10c11765ad9
Mark Shwartzman <mark@bitclave.com>
2019-06-12 17:11:30 -0700

    increase page size

--- src/main/java/com/bitclave/matcher/BaseClient.java
+++ src/main/java/com/bitclave/matcher/BaseClient.java
@@ -72,7 +72,7 @@ public class BaseClient {
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
-    params.put("size", 5100);
+    params.put("size", 2100);
 
     while(pageThrough) {
       ResponseEntity<PagedResponse<OfferSearch>> offerSearchResponse =
