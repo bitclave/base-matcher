cb55abba1d63d6887750d5a9dc905333cd24aa06
Mark Shwartzman <mark@bitclave.com>
2019-06-14 16:52:18 -0700

    update page size

--- src/main/java/com/bitclave/matcher/BaseClient.java
+++ src/main/java/com/bitclave/matcher/BaseClient.java
@@ -52,7 +52,7 @@ public class BaseClient {
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
-    params.put("size", 2100);
+    params.put("size", 1100);
 
     while(pageThrough) {
       ResponseEntity<PagedResponse<Offer>> offerResponse =
@@ -72,7 +72,7 @@ public class BaseClient {
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
-    params.put("size", 2100);
+    params.put("size", 1100);
 
     while(pageThrough) {
       ResponseEntity<PagedResponse<OfferSearch>> offerSearchResponse =
@@ -92,7 +92,7 @@ public class BaseClient {
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
-    params.put("size", 2100);
+    params.put("size", 1100);
 
     while(pageThrough) {
       ResponseEntity<PagedResponse<SearchRequest>> searchRequestResponse =
