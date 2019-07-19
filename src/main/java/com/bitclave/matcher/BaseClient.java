677ee0f801114bd90b658cbba6db77b6115a5c2a
AndreyShashlov <andrey@bitclave.com>
2019-07-19 11:55:30 +0300

    repeat request of sync

--- src/main/java/com/bitclave/matcher/BaseClient.java
+++ src/main/java/com/bitclave/matcher/BaseClient.java
@@ -31,6 +31,7 @@ import static com.bitclave.matcher.models.SignedRequest.newSignedRequest;
 @Service
 public class BaseClient {
   private static final Logger log = LoggerFactory.getLogger(BaseClient.class);
+  private static final int MAX_REPEAT_COUNT = 5;
 
   @Value("${matcher.base.url}")
   private String rootUri;
@@ -49,66 +50,105 @@ public class BaseClient {
   }
 
   public List<Offer> offers() {
+    int repeat = 0;
     List<Offer> allOffers = new ArrayList<>();
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
     params.put("size", 255);
 
-    while(pageThrough) {
-      ResponseEntity<SliceResponse<Offer>> offerResponse =
-              restTemplate.exchange("/v1/consumers/offers?page={page}&size={size}",
-                      HttpMethod.GET, null,
-                      new ParameterizedTypeReference<SliceResponse<Offer>>() {}, params);
+    while (pageThrough) {
+      try {
+        ResponseEntity<SliceResponse<Offer>> offerResponse =
+          restTemplate.exchange("/v1/consumers/offers?page={page}&size={size}",
+            HttpMethod.GET, null,
+            new ParameterizedTypeReference<SliceResponse<Offer>>() {
+            }, params);
 
-      allOffers.addAll(offerResponse.getBody().getContent());
-      pageThrough = offerResponse.getBody().hasNext();
-      params.put("page", params.get("page") + 1);
+        allOffers.addAll(offerResponse.getBody()
+                                      .getContent());
+        pageThrough = offerResponse.getBody()
+                                   .hasNext();
+        params.put("page", params.get("page") + 1);
+        repeat = 0;
+      } catch (Throwable e) {
+        repeat++;
+        log.warn("offers", e);
+        if (repeat > MAX_REPEAT_COUNT) {
+          throw e;
+        }
+      }
     }
     return allOffers;
   }
 
   public List<OfferSearch> offerSearches() {
+    int repeat = 0;
     List<OfferSearch> allOfferSearches = new ArrayList<>();
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
     params.put("size", 255);
 
-    while(pageThrough) {
-      ResponseEntity<SliceResponse<OfferSearch>> offerSearchResponse =
-              restTemplate.exchange("/v1/consumers/search/results?page={page}&size={size}",
-                      HttpMethod.GET, null,
-                      new ParameterizedTypeReference<SliceResponse<OfferSearch>>() {}, params);
+    while (pageThrough) {
+      try {
+        ResponseEntity<SliceResponse<OfferSearch>> offerSearchResponse =
+          restTemplate.exchange("/v1/consumers/search/results?page={page}&size={size}",
+            HttpMethod.GET, null,
+            new ParameterizedTypeReference<SliceResponse<OfferSearch>>() {
+            }, params);
 
-      allOfferSearches.addAll(offerSearchResponse.getBody().getContent());
-      pageThrough = offerSearchResponse.getBody().hasNext();
-      params.put("page", params.get("page") + 1);
+        allOfferSearches.addAll(offerSearchResponse.getBody()
+                                                   .getContent());
+        pageThrough = offerSearchResponse.getBody()
+                                         .hasNext();
+        params.put("page", params.get("page") + 1);
 
-      // temp workaround to limit the number of pages till we resolve BASE-760
+        // temp workaround to limit the number of pages till we resolve BASE-760
 //      if ((params.get("page") * params.get("size"))> 500000) {
 //        pageThrough = false;
 //      }
+
+      } catch (Throwable e) {
+        repeat++;
+        log.warn("offerSearches", e);
+        if (repeat > MAX_REPEAT_COUNT) {
+          throw e;
+        }
+      }
     }
     return allOfferSearches;
   }
 
   public List<SearchRequest> searchRequests() {
+    int repeat = 0;
     List<SearchRequest> allRequests = new ArrayList<>();
     boolean pageThrough = true;
     Map<String, Integer> params = new HashMap<>();
     params.put("page", 0);
     params.put("size", 255);
 
-    while(pageThrough) {
-      ResponseEntity<SliceResponse<SearchRequest>> searchRequestResponse =
-              restTemplate.exchange("/v1/consumers/search/requests?page={page}&size={size}",
-                      HttpMethod.GET, null,
-                      new ParameterizedTypeReference<SliceResponse<SearchRequest>>() {}, params);
+    while (pageThrough) {
+      try {
+        ResponseEntity<SliceResponse<SearchRequest>> searchRequestResponse =
+          restTemplate.exchange("/v1/consumers/search/requests?page={page}&size={size}",
+            HttpMethod.GET, null,
+            new ParameterizedTypeReference<SliceResponse<SearchRequest>>() {
+            }, params);
 
-      allRequests.addAll(searchRequestResponse.getBody().getContent());
-      pageThrough = searchRequestResponse.getBody().hasNext();
-      params.put("page", params.get("page") + 1);
+        allRequests.addAll(searchRequestResponse.getBody()
+                                                .getContent());
+        pageThrough = searchRequestResponse.getBody()
+                                           .hasNext();
+        params.put("page", params.get("page") + 1);
+
+      } catch (Throwable e) {
+        repeat++;
+        log.warn("searchRequests", e);
+        if (repeat > MAX_REPEAT_COUNT) {
+          throw e;
+        }
+      }
     }
     return allRequests;
   }
@@ -133,7 +173,8 @@ public class BaseClient {
   }
 
   private ResponseEntity<OfferSearch> saveOfferSearch(AtomicLong nonce, OfferSearch offerSearch) {
-    final SignedRequest<OfferSearch> signedRequest = newSignedRequest(offerSearch, publicKey, nonce.incrementAndGet());
+    final SignedRequest<OfferSearch> signedRequest = newSignedRequest(offerSearch, publicKey,
+      nonce.incrementAndGet());
     signedRequest.signMessage(privateKey);
 
     log.info("Saving offerSearch to BASE: {}", signedRequest.toString());
